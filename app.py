83c7862ea694a1685785ed999f402e08e46c1e13
FLY100% <panapot@hotmail.com>
2018-08-17 13:09:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,194 +1,77 @@
 # -*- coding: utf-8 -*-
 from flask import Flask 
-# redirect, url_for
 
 #=== TO do cross-origin AJAX possible ===#
-from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_restful import Api, Resource, reqparse, abort
 
 #=== To do handle data ===#
 from collections import Counter
 import datetime
-import config
 
 #=== normalization ===#
 import numpy as np
 import pandas as pd
 
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
 app = Flask(__name__)
-# To allow CORS for all domains on all routes.
-CORS(app)
-APP_CONFIG = config.APP_CONFIG
-PRODUCTION = config.PRODUCTION
-
-if PRODUCTION:
-    CPU_CORE = config.CPU_CORE
-    NUM_ROOM = config.NUM_ROOM
-    NUM_TAG = config.NUM_TAG
-    NUM_FOLLOWING = config.NUM_FOLLOWING
-    NUM_NEIGHBOUR = config.NUM_NEIGHBOUR
-    NUM_TOPIC = config.NUM_TOPIC
-
-#=== mongoDB Config ===#
-env = config.ENV_CONFIG
-if env == "DevConfig":
-    DATABASE_CONFIG = config.DevConfig.DATABASE_CONFIG
-    dbname = config.DevConfig.DBNAME
-elif env == "ProductConfig":
-    DATABASE_CONFIG = config.ProductConfig.DATABASE_CONFIG
-    dbname = config.ProductConfig.DBNAME
-else:
-    print("Error Env.")
-    raise ValueError
-
-
-app.config['MONGO_HOST'] = DATABASE_CONFIG[dbname]['host']
-app.config['MONGO_PORT'] = DATABASE_CONFIG[dbname]['port']
-app.config['MONGO_DBNAME'] = dbname
-app.config['MONGO_USERNAME'] = DATABASE_CONFIG[dbname]['username']
-app.config['MONGO_PASSWORD'] = DATABASE_CONFIG[dbname]['password']
-mongo = PyMongo(app, config_prefix='MONGO')
-
-
-def abort_if_user_doesnt_exist(_id):
-    col_cf_user_profile = mongo.db.cf_user_profile_parallel
-    if col_cf_user_profile.find({'mid': _id}, {'_id': 1}).limit(1) is None:
-        abort(404, message="user {} doesn't exist".format(_id))
-
-
-def check_cold_start(_id):
-    col_cf_user_profile = mongo.db.cf_user_profile_parallel
-    if len(list(col_cf_user_profile.find({'mid': _id}, {'stat': 1}).limit(1))['stat']) < 2:
-        abort(404, message="user {} new user".format(uid))
-
-
-class UserBehavior(Resource):
-    def get(self, mid=None, cookie=None):
-        col_pt_cs_day = mongo.db.pt_cs_day_test
-        col_pt_user_behavior_global = mongo.db.pt_user_behavior_global
-        col_members = mongo.db.members
-        col_pt_Gender_Prediction = mongo.db.pt_Gender_Prediction
-        col_topics = mongo.db.topics
-        return
-   
-
-class UserBehaviorStats(Resource):
-    def get(self, mid=None, cid=None):
-        return
-
-
-class UserBehaviorRooms(Resource):
-    def __init__(self):
-        self.col_cf_user_profile_parallel = mongo.db.cf_user_profile_parallel
-        self.room = {
-            'mid': 1,
-            'cookie': 1,
-            'rooms_tf': 1
-        }
-        self.status = 'one-many'
-
-    def get(self, mid=None, cid=None):
-        return
-
-
-class UserBehaviorTags(Resource):
-    def get(self, mid=None, cid=None):
-        check_cold_start(uid)
-        return
-
-
-# user_behavior (following)
-class UserBehaviorFollowing(Resource):
-    def __init__(self):
-        self.col_members = mongo.db.members
-
-    def get(self, mid=None, cid=None):
-        print(mid)
-        print(cookie)
-        if mid:
-            # result = col_members.find().count()
-            result = self.col_members.find_one({'_id': mid})
-            if result:
-                # return jsonify({'result': result})
-                return {'result': result}
-            else:
-                return {'response': "mid missing"}
-        else:
-            data = {'response': "ERROR"}
-            return data
-
-
-class UserRecommendTopics(Resource):
-    # TODO find topic list from neighbour
-    def get(self, mid=None, cid=None):
-        uid = mid or cid
-        return {'data': uid}
-
-
-#=== Rounting ===#
 api = Api(app)
-# cursor.skip()
-# cursor.limit()
 
-#=== Behavior ===#
-# Stats
-api.add_resource(
-    UserBehaviorStats,
-    "/api/behavior/stats/<int:mid>",
-    "/api/behavior/stats/<string:cid>",
-    endpoint="behavioral_stats"
-)
-# Rooms
-api.add_resource(
-    UserBehaviorRooms,
-    "/api/behavior/rooms/<int:mid>",
-    "/api/behavior/rooms/<string:cookie>",
-    endpoint="behavioral_rooms"
-)
-# Tags
-api.add_resource(
-    UserBehaviorTags,
-    "/api/behavior/tags/<int:mid>",
-    "/api/behavior/tags/<string:cookie>",
-    endpoint="behavioral_tags"
-)
-# Following
-api.add_resource(
-    UserBehaviorFollowing,
-    "/api/behavior/following/<int:mid>",
-    "/api/behavior/following/<string:cookie>",
-    endpoint='behavioral_following'
-)
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
 
-#=== Recommend ===#
-# Topics
-api.add_resource(
-    UserRecommendTopics,
-    "/api/recommend/topics/<int:mid>",
-    "/api/recommend/topics/<string:cookie>",
-    endpoint='recommend_topics'
-)
 
-# Neighbours
-api.add_resource(
-    UserRecommendNeighbours,
-    "/api/recommend/neighbours/<int:mid>",
-    "/api/recommend/neighbours/<string:cookie>",
-    endpoint='recommend_neighbours'
-)
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-# top@member
-api.add_resource(
-    TopMember,
-    "/api/top/member/",
-    endpoint="top_member"
-)
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
-if __name__ == "__main__":
-    app.run(
-        host=APP_CONFIG['host'],
-        port=APP_CONFIG['port'],
-        threaded=APP_CONFIG['threaded'],
-        debug=APP_CONFIG['debug']
-    )
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
